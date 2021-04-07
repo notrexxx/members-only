355818bcf088a2c7fcba4d50871acfdb846b513d
notrexxx <notrexx15@gmail.com>
2021-04-07 19:58:40 -0400

    Delete unnecessary files and correct linter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,18 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [ :new, :create ]
+  before_action :authenticate_user!, only: %i[new create]
 
   # GET /posts or /posts.json
   def index
     @posts = Post.all
   end
 
-  def show
-  end
-
+  def show; end
 
   def new
     @post = Post.new
   end
 
-
-  def edit
-  end
-
+  def edit; end
 
   def create
     @post = current_user.posts.build(post_params)
@@ -32,7 +27,7 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to @post, notice: "Post was successfully updated." }
+        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
         format.json { render :show, status: :ok, location: @post }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -44,19 +39,18 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
+      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    
-    def set_post
-      @post = Post.find(params[:id])
-    end
 
-    
-    def post_params
-      params.require(:post).permit(:title, :body)
-    end
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
