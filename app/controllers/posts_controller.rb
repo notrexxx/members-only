bb9821e2bca1ee3cfc78e75db0983f6fa3cc2cd4
notrexxx <notrexx15@gmail.com>
2021-04-07 20:10:15 -0400

    Remove unnecessary controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: %i[new create]
 
-  # GET /posts or /posts.json
   def index
     @posts = Post.all
   end
@@ -24,25 +23,9 @@ class PostsController < ApplicationController
     end
   end
 
-  def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  def update; end
 
-  def destroy
-    @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  def destroy; end
 
   private
 
